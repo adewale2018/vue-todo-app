cf7e595be9cd4af89b810190de2b683da5be5770
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-06-02 02:00:12 +0100

    set up the project, add todos and todoItem components, implement delete todo functionality

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
+</template>
+
+<script>
+import Todos from "./components/Todos";
+
+export default {
+  name: "App",
+  components: {
+    Todos
+  },
+  methods: {
+    deleteTodo: function(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    }
+  },
+  data: function() {
+    return {
+      todos: [
+        { id: 1, task: "Learn JavaScript ES6", done: true },
+        { id: 2, task: "Learn React", done: false },
+        { id: 3, task: "Learn Block-chain technology", done: true },
+        { id: 4, task: "Attend a meet up", done: true }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-completed': todo.done}">
+    <ul>
+      <li>
+        <input type="checkbox" v-on:change="markCompleted" />
+        {{ todo.task}}
+        <button @click="$emit('del-todo', todo.id)" class="del">X</button>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+
+  methods: {
+    markCompleted: function() {
+      this.todo.done = !this.todo.done;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px #ccc dotted;
+}
+.is-completed {
+  text-decoration: line-through;
+}
+.del {
+  background: #ff0000;
+  color: #fff;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+}
+</style>
